17424da8d67637ce36dfb4d8b6f060bd10845612
sandeep-chauhan-self <scst1011@gmail.com>
2025-12-21 05:07:31 +0530

    chore: sort imports alphabetically

--- frontend/src/components/BacktestResults.js
+++ frontend/src/components/BacktestResults.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate, useSearchParams, useLocation } from 'react-router-dom';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import './BacktestResults.css';
 
 // Strategy definitions with descriptions
